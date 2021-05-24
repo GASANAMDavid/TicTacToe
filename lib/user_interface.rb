6fba99a71bafeacc9db9eff10f95db962557d960
David Gasana Manzi <gmdavid59@gmail.com>
2021-05-24 08:06:25 +0200

    choosing symbols

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,10 +1,18 @@
 module UserInterface
   def self.display_instructions(output = $stdout)
     output.puts "\n\n  Instructions\n::::::::::::::::\n\n"
-    output.puts " 1. Players choose a unique symbol as a mark to use throught the game.\n"
+    output.puts " 1. Players choose a unique symbol to use as a mark throught the game.\n"
     output.puts " 2. The game is played on a grid of 3 by 3.\n"
     output.puts " 3. Players take turns putting their marks into empty squares.\n"
     output.puts " 4. The first player to put 3 symbols in a any of 3 rows and 3 columns or any of the diagnols is the WINNER.\n"
     output.puts " 5. If all 9 squares are full and there is no winner the game is TIED and Over.\n\n"
+  end
+
+  def self.player_symbols(output, input)
+    output.puts "Enter player's name."
+    name = input.gets.chomp
+    output.puts "Enter player's symbol."
+    symbol = input.gets.chomp
+    { name => symbol }
   end
 end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -3,8 +3,14 @@ require 'stringio'
 
 RSpec.describe UserInterface do
   let(:output) { StringIO.new }
+  let(:input) { StringIO.new("Bob\nX\nAlice\nO\n") }
   it 'displays instructions to the users of the game' do
     subject.display_instructions(output)
-    expect(output.string).to include("Instructions\n::::::::::::::::\n\n 1. Players choose a symbol to use throught the game.")
+    expect(output.string).to include("Instructions\n::::::::::::::::\n\n 1. Players choose a unique symbol to use as a mark throught the game.")
+  end
+
+  it 'allows users of the game to choose the symbols' do
+    subject.player_symbols(output, input)
+    expect(output.string).to include("Enter player's name.")
   end
 end

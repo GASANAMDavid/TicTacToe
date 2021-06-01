2b8e58a92dbea67bcac351bb4944dd6ea090d814
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-01 12:56:07 +0200

    checks winner or tie

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,14 @@
+require_relative 'express_board_in_rows'
+
 class Board
+  include BoardasRows
+
   DENOTE_EMPTY = '-'
-  attr_reader :board, :num_of_blank_spaces, :blank_positions
+  attr_reader :board, :num_of_played_positions, :blank_positions
 
   def initialize(size)
     @board = Array.new(size) { Array.new(size) { DENOTE_EMPTY } }
-    @num_of_blank_spaces = size * size
+    @num_of_played_positions = 0
     @blank_positions = make_free_positions_hash(size.times.to_a.product(size.times.to_a))
   end
 
@@ -16,7 +20,7 @@ class Board
 
   def apply_move(symbol, position)
     board[position[:row]][position[:col]] = symbol
-    @num_of_blank_spaces -= 1
+    @num_of_played_positions += 1
     blank_positions.delete(blank_positions.key(position))
   end
 
@@ -26,5 +30,23 @@ class Board
       hash[i + 1] = { row: blanks[i][0], col: blanks[i][1] }
     end
     hash
+  end
+
+  def board_state(symbol)
+    return nil if num_of_played_positions < board.size * 2 - 1
+
+    check_rows = BoardasRows.express_board_as_rows(board)
+
+    if check_rows.any? { |row| check_uniqueness(row, symbol) }
+      'Winner'
+    elsif num_of_played_positions == board_size * board_size
+      'Tie'
+    end
+  end
+
+  private
+
+  def check_uniqueness(row, mark)
+    row.uniq.count == 1 && row[0] == mark
   end
 end
---
+++ lib/express_board_in_rows.rb
@@ -0,0 +1,20 @@
+module BoardasRows
+  def self.express_board_as_rows(board)
+    all_as_rows = []
+    board.each do |row|
+      all_as_rows << row
+    end
+
+    board.transpose.each do |row|
+      all_as_rows << row
+    end
+
+    all_as_rows << (0...board.size).collect do |item|
+      board[item][item]
+    end
+
+    all_as_rows << (0...board.size).collect do |item|
+      board[item][board.size - (item + 1)]
+    end
+  end
+end

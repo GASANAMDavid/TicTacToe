2d157729000732e9b53f581d1f5213a341c3fed0
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-02 10:51:07 +0200

    clarifying names for similar variables

--- lib/board.rb
+++ lib/board.rb
@@ -8,7 +8,7 @@ class Board
   def initialize(size)
     @board = Array.new(size) { Array.new(size) { DENOTE_EMPTY } }
     @num_of_played_positions = 0
-    @blank_positions = make_free_positions_hash(size.times.to_a.product(size.times.to_a))
+    @blank_positions = make_blank_positions_hash(size.times.to_a.product(size.times.to_a))
   end
 
   def print_board(output = $stdout)
@@ -23,7 +23,7 @@ class Board
     blank_positions.delete(blank_positions.key(position))
   end
 
-  def make_free_positions_hash(blanks)
+  def make_blank_positions_hash(blanks)
     hash = {}
     (0...blanks.length).each do |i|
       hash[i + 1] = { row: blanks[i][0], col: blanks[i][1] }

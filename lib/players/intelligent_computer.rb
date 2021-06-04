9b6232283090da9636e0233a2815dc72c2f0d738
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-04 12:09:44 +0200

    so far two passing tests

--- lib/players/intelligent_computer.rb
+++ lib/players/intelligent_computer.rb
@@ -2,17 +2,32 @@ require_relative 'player'
 
 class IntelligentComputer < Player
   def make_move(playing_board)
-    depth = playing_board.board.size
-    minimax(playing_board, depth, true)
+    # depth = playing_board.board.size
+    minimax(playing_board, 1, 'O')
   end
 
-  def minimax(current_board, depth, _is_minimizing)
-    return +1 if current_board.board_state('X') == 'Winner'
-    return -1 if current_board.board_state('O') == 'Winner'
-    return 0 if current_board.board_state('X') == 'Tie'
-    return 0 if current_board.board_state('O') == 'Tie'
+  def minimax(current_board, depth, symbol)
+    return compute_score(current_board, symbol) if depth.zero?
 
-    current_board.apply_move(9, 'X')
-    minimax(current_board, depth - 1, false)
+    scores = []
+    moves = []
+    available_positions = current_board.blank_positions
+    available_positions.each do |key, _indices|
+      current_board.apply_move(symbol, key)
+      score = minimax(current_board, depth - 1, 'X')
+      scores << score unless score.nil?
+      moves << key
+    end
+
+    puts current_board.blank_positions
+    maximum_score_index = scores.each_with_index.max[1]
+    moves[maximum_score_index]
+  end
+
+  def compute_score(current_board, symbol)
+    return +1 if current_board.board_state(symbol) == 'Winner'
+    return -1 if current_board.board_state(symbol) == 'Winner'
+    return 0 if current_board.board_state(symbol) == 'Tie'
+    return 0 if current_board.board_state(symbol) == 'Tie'
   end
 end
--- spec/players_spec/intelligent_computer_spec.rb
+++ spec/players_spec/intelligent_computer_spec.rb
@@ -3,9 +3,9 @@ require_relative '../../lib/board'
 
 RSpec.describe IntelligentComputer do
   describe '#make_move' do
-    context 'starting from leaf nodes one play away from finishing the game' do
-      let(:subject) { IntelligentComputer.new('Intelligent Computer', 'O') }
-      let(:playing_board) { Board.new(3) }
+    let(:subject) { IntelligentComputer.new('Intelligent Computer', 'O') }
+    let(:playing_board) { Board.new(3) }
+    context 'starting from leaf nodes with one play away from finishing the game' do
       before do
         [
           [[1, 3, 5, 8], 'X'],
@@ -17,8 +17,26 @@ RSpec.describe IntelligentComputer do
         end
       end
 
+      it 'returns position key 9 from available positions ' do
+        expect(subject.make_move(playing_board)).to eq 9
+      end
+    end
+    context 'starting from leaf nodes with three plays away from finishing the game' do
+      before do
+        [
+          [[1, 3, 5], 'X'],
+          [[4, 2, 6], 'O']
+        ].each do |moves, symbol|
+          moves.each do |move|
+            playing_board.apply_move(symbol, move)
+          end
+        end
+      end
+
       it 'returns 1 ' do
-        expect(subject.make_move(playing_board)).to eq 1
+        expect(subject.make_move(playing_board)).to eq 7
+        puts playing_board.board.to_s
+        puts playing_board.blank_positions
       end
     end
   end

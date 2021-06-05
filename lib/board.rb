6d277a8b1e99935859af268beb23363e7da7b5a0
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-05 16:11:28 +0200

    implementated blank_postions to dynamically return blank_spaces

--- lib/board.rb
+++ lib/board.rb
@@ -8,7 +8,6 @@ class Board
   def initialize(size)
     @board = Array.new(size) { Array.new(size) { DENOTE_EMPTY } }
     @num_of_played_positions = 0
-    @blank_positions = make_blank_positions_hash(size.times.to_a.product(size.times.to_a))
   end
 
   def apply_move(symbol, move)
@@ -16,14 +15,6 @@ class Board
     board[position[:row]][position[:col]] = symbol
     @num_of_played_positions += 1
     blank_positions.delete(blank_positions.key(position))
-  end
-
-  def make_blank_positions_hash(blanks)
-    hash = {}
-    (0...blanks.length).each do |i|
-      hash[i + 1] = { row: blanks[i][0], col: blanks[i][1] }
-    end
-    hash
   end
 
   def blank_positions

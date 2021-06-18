94e93311eb14583af4a500d6b395bb8bd5a52d92
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-18 09:59:01 +0200

    add path to SetLanguages  modules in tictactoe file

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -1,7 +1,8 @@
 require_relative 'tictactoe/version'
+require_relative '../config/set_languages'
 
-module Hello
-  def self.hello
-    puts 'Hello World!'
-  end
-end
+# module Hello
+#   def self.hello
+#     puts 'Hello World!'
+#   end
+# end

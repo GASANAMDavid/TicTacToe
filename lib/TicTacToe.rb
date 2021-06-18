59071af7d51913607437549e6720bc3e4b7eeb92
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-18 14:17:37 +0200

    testing a gem fix

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -9,7 +9,7 @@ require_relative './players/human'
 require_relative './players/intelligent_computer'
 require_relative './express_board_in_rows'
 # require_relative '../config/env_variables'
-require_relative './../config/game_interface'
+require_relative 'config/game_interface'
 # require_relative '../config/game_set_up'
 # SetLanguages.change_language('fr')
 # GameInterface.display_language_options

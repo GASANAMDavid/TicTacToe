19bc2fb021e0381b3bf19cf4eb37af2f945542fd
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-18 15:48:28 +0200

    Testing gem

--- config/env_variables.rb
+++ config/env_variables.rb
@@ -2,6 +2,8 @@ require 'i18n'
 require 'dotenv'
 # puts 'Test env variables'
 Dotenv.load
-# I18n.load_path << Dir["#{File.expand_path('config/locales')}/*.yml"]
+path = Dir["#{File.expand_path('config/locales')}/*.yml"]
+puts path
+# I18n.load_path << 
 # I18n.default_locale = 'en'
-puts 'after everything'
+# puts 'after everything'

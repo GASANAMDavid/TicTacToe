29766c9c975bdb69de513be9a7dcfe9ae40da72b
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-18 13:06:52 +0200

    Adding necessary interfaces to be available in the api

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -1,9 +1,12 @@
 require_relative 'TicTacToe/version'
+require_relative './../config/set_language'
 require_relative './user_interface'
+require_relative './validation'
+require_relative './game_status'
+require_relative 'game_engine'
+require_relative './board'
+require_relative './players/human'
+require_relative './players/intelligent_computer'
+
 
 # Testing exposed interfaces in the gem
-module Hello
-  def self.hello
-    puts 'Hello World!'
-  end
-end

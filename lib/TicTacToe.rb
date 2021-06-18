bf6b099f942e7a2b38355fe9cc43976d98cf4a8e
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-18 11:03:53 +0200

    test

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -1,8 +1,8 @@
 require_relative 'tictactoe/version'
-require_relative '../config/set_languages'
+# require_relative '../config/set_languages'
 
-# module Hello
-#   def self.hello
-#     puts 'Hello World!'
-#   end
-# end
+module Hello
+  def self.hello
+    puts 'Hello World!'
+  end
+end

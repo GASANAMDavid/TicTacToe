438195d70877f422308976ffe0f90b9eba30b758
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-19 16:37:50 +0200

    Testing gem

--- config/env_variables.rb
+++ config/env_variables.rb
@@ -1,5 +1,6 @@
 require 'i18n'
 require 'dotenv'
 Dotenv.load
-path = Dir.glob("#{File.dirname(__FILE__)}/config/locales/**.yml")
+path = Dir.glob("#{File.dirname(__FILE__)}/locales/**.yml")
+puts path
 I18n.load_path += path

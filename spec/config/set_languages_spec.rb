947dbe39ddea2c89207fa61bad3588d0d1245c27
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-25 15:15:07 +0200

    namespacing

--- spec/config/set_languages_spec.rb
+++ spec/config/set_languages_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../../config/set_languages'
 
-RSpec.describe SetLanguages do
+RSpec.describe TicTacToe::SetLanguages do
   context '.change_language' do
     it 'changes the language used in the game' do
-      SetLanguages.change_language
+      TicTacToe::SetLanguages.change_language
       expect(I18n.default_locale).to eq :en
     end
 
     it 'sets language from argument list' do
-      SetLanguages.change_language('en')
+      TicTacToe::SetLanguages.change_language('en')
       expect(I18n.default_locale).to eq :en
     end
   end

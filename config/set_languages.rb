69a453760a5e1ab9b877291f52fb99ae5d8c7fad
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-25 15:13:04 +0200

    updated version

--- config/set_languages.rb
+++ config/set_languages.rb
@@ -1,8 +1,10 @@
 require_relative './game_interface'
-module SetLanguages
-  def self.change_language(lang = 'en')
-    I18n.default_locale = lang
-  rescue StandardError => e
-    GameInterface.display_error_message(e.message)
+module TicTacToe
+  module SetLanguages
+    def self.change_language(lang = 'en')
+      I18n.default_locale = lang
+    rescue StandardError => e
+      GameInterface.display_error_message(e.message)
+    end
   end
 end

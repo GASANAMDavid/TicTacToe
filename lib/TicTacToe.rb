40b2ebbf006b328bd259be463bd47b2bffb355ea
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-28 10:32:27 +0200

    gem fix

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -11,3 +11,5 @@ require_relative './express_board_in_rows'
 require_relative '../config/env_variables'
 require_relative './game_config/game_interface'
 require_relative './game_config/game_set_up'
+
+

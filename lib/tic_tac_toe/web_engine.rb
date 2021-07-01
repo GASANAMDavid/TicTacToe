a1e0fe2f26c4632565538961a7e3d666ae7e480e
David Gasana Manzi <gmdavid59@gmail.com>
2021-07-01 11:59:21 +0200

    change the gem to have one interface on the API

--- lib/tic_tac_toe/web_engine.rb
+++ lib/tic_tac_toe/web_engine.rb
@@ -1,21 +1,21 @@
 require_relative './game_config/game_interface'
 require_relative './board'
 require_relative './game_status'
+require_relative './opponent_type'
 module TicTacToe
   class WebEngine
     attr_reader :board
 
-    def initialize(board, current_player_name, opponent_player)
+    def initialize(board, current_player_name, game_mode)
       @board = board
       @game_board = TicTacToe::Board.new(board.size)
       @game_board.board = board
-      @opponent_player = opponent_player
+      @opponent_player = TicTacToe::OpponentType.when_game_mode_is(game_mode)
       @current_player_name = current_player_name
     end
 
     def play(symbol, move)
       @game_board.apply_move(symbol, move)
-
       opponent_player_move if @game_board.board_state(symbol).nil?
       @board = @game_board.board
     end
--- spec/web_engine_spec.rb
+++ spec/web_engine_spec.rb
@@ -3,7 +3,8 @@ require_relative '../lib/tic_tac_toe/web_engine'
 RSpec.describe TicTacToe::WebEngine do
   let(:board) { [['X', '-', '-'], ['-', '-', '-'], ['-', '-', '-']] }
   let(:opponent_player) { double('RandomComputer', name: 'Random Computer', symbol: 'O', make_move: 9) }
-  let(:web_engine) { TicTacToe::WebEngine.new(board, 'David', opponent_player) }
+  let(:game_mode) { 2 }
+  let(:web_engine) { TicTacToe::WebEngine.new(board, 'David', game_mode) }
   it 'creates a board with assigned values' do
     expect(web_engine.board).to eq(board)
   end
@@ -16,17 +17,19 @@ RSpec.describe TicTacToe::WebEngine do
 
     context 'when the game is not finished after current_players move' do
       it 'allows opponent_player to make move' do
-        web_engine.play('X', 2)
-        allow(opponent_player).to receive(:make_move)
-        expect(web_engine.board[2][2]).to eq('O')
+        web_engine.board[0][0] = '-'
+        web_engine.play('X', 1)
+        expect(web_engine.board[1][1]).to eq('O')
       end
     end
 
     context 'when the game is finished after current_players move' do
       it "doesn't allow opponent_player to make move" do
-        board[0][2]
-        web_engine.play('X', 2)
-        expect(opponent_player).not_to receive(:make_move)
+        web_engine.board[1][1] = 'O'
+        web_engine.board[0][2] = 'O'
+        web_engine.board[1][0] = 'X'
+        web_engine.play('X', 7)
+        expect(web_engine.board).to eq([['X', '-', 'O'], ['X', 'O', '-'], ['X', '-', '-']])
       end
     end
   end

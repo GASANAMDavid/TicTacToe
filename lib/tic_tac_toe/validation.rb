b5043b81e69b836069d44ed31b970a73c3440b18
David Gasana Manzi <gmdavid59@gmail.com>
2021-07-07 10:34:54 +0200

    custom exception

--- lib/tic_tac_toe/validation.rb
+++ lib/tic_tac_toe/validation.rb
@@ -1,9 +1,11 @@
 module TicTacToe
+  class InvalidMove < StandardError; end
+
   module Validation
     def self.validate_move(board, move)
       blank_spaces = board.blank_positions
       key = blank_spaces.key?(move)
-      raise StandardError, 'Invalid move' unless key
+      raise InvalidMove, 'Invalid move' unless key
 
       key
     end
